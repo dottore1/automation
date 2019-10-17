0bbf9d4e7eabbf51ac755aac6d013d6de3a03559
dottore1 <54813290+dottore1@users.noreply.github.com>
2019-10-18 01:08:56 +0200

    upload 2 scripts

---
+++ Organizer(overførsler).py
@@ -0,0 +1,39 @@
+import os
+from pathlib import Path
+
+DIRECTORIES = {
+    "DOCUMENTS": [".pdf", ".docx", ".xlsx", ".txt"],
+    "PICTURES": [".jpg", ".png", ".gif", ".psd", ".PNG"],
+    "INSTALLERS": [".exe"],
+    "WEB": [".html",".xml", ".js"]
+}
+
+
+FILE_FORMATS = {file_format: directory
+                for directory, file_formats in DIRECTORIES.items()
+                for file_format in file_formats}
+#This will organise your files
+def organize():
+    for entry in os.scandir():
+        if entry.is_dir():
+            continue
+        file_path = Path(entry.name)
+        file_format = file_path.suffix.lower()
+        if file_format in FILE_FORMATS:
+            directory_path = Path(FILE_FORMATS[file_format])
+            directory_path.mkdir(exist_ok=True)
+            file_path.rename(directory_path.joinpath(file_path))
+    try:
+        os.mkdir("OTHERS")
+    except:
+        pass
+    for dir in os.scandir():
+        try:
+            if dir.is_dir():
+                os.rmdir(dir)
+            else:
+                os.rename(os.getcwd() + '/' + str(Path(dir)), os.getcwd() + '/OTHER/' + str(Path(dir)))
+        except:
+            pass
+if __name__ == "__main__":
+    organize()
---
+++ pdfseracher_v2.py
@@ -0,0 +1,33 @@
+import PyPDF2
+import sys
+import time
+import os
+
+print("Welcome to the pdf searcher! \nI can search in x pdf files given in a specific folder :-)")
+search_term = input(str("Type in the keyword you want to search for: "))
+
+
+def searcher(path, keyword):
+    pdfFileObj = open(str(path) , 'rb')
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj, strict=True)
+    search_word = keyword
+    search_word_count = 0
+
+    for pageNum in range(1, pdfReader.numPages):
+        pageObj = pdfReader.getPage(pageNum)
+        text = pageObj.extractText().encode('utf-8')
+        search_text = text.lower().split()
+        for word in search_text:
+            if search_word in word.decode("utf-8"):
+                search_word_count += 1
+                print("Keyword: " + search_word + " is found on " + "page number: " + str(pdfReader.getPageNumber(pageObj)+1))
+
+    print("The keyword {} was found {} times".format(search_word, search_word_count) + " in " + str(pdfFileObj.name))
+    time.sleep(1)
+
+
+for file in os.listdir("C:/Users/Nichlas/Documents/Noter_OOP/search_area"):
+    if file.endswith(".pdf"):
+        path =  os.path.join("C:/Users/Nichlas/Documents/Noter_OOP/search_area", file)
+        print("Now searching: " + path)
+        searcher(path, search_term)
